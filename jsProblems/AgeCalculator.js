0debadf662058c38829dee3cb7c1e6e35b2f91a5
guohao.yan <sccdmaomao@hotmail.com>
2022-10-17 22:13:47 -0400

    add js problems

---
+++ jsProblems/AgeCalculator.js
@@ -0,0 +1,12 @@
+
+/**
+ * Given any dob, calculates the current age.
+ * return -1 if dob is invalid or at a future date.
+ * For example: calculateAge("1994-04-05") should return 28
+ * @param {string} dob date of birth string. Format: YYYY-MM-DD
+ */
+function calculateAge(dob) {
+    return -1
+}
+
+export default calculateAge;
---
+++ jsProblems/KmToMileConverter.js
@@ -0,0 +1,11 @@
+
+/**
+ * Convert KM unit to Mile, rounded down.
+ * For example: KmToMileConverter(100) should return 62
+ * @param {number} km number of kilometer
+ */
+function KmToMileConverter(km) {
+    return 0
+}
+
+export default KmToMileConverter;
