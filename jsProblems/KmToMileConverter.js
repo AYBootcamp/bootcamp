ea29a467a827ee3fe688d9d3c28ec3726966cb59
guohao.yan <sccdmaomao@hotmail.com>
2022-10-17 22:15:31 -0400

    add hint

--- jsProblems/KmToMileConverter.js
+++ jsProblems/KmToMileConverter.js
@@ -2,6 +2,7 @@
 /**
  * Convert KM unit to Mile, rounded down.
  * For example: KmToMileConverter(100) should return 62
+ * 1 kilometer (km) = 0.62137 miles (mi)
  * @param {number} km number of kilometer
  */
 function KmToMileConverter(km) {
