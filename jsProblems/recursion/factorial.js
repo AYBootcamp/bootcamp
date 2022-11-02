4b8599c3d69d05d3344b94ecbade4db93094806d
guohao.yan <sccdmaomao@hotmail.com>
2022-11-02 18:55:31 -0400

    add recursion problems

---
+++ jsProblems/recursion/factorial.js
@@ -0,0 +1,9 @@
+/**
+ * Return the factorial of a given number.
+ * factorial of 5 = 5! = 5*4*3*2*1
+ * @param {*} numb 
+ * @returns 
+ */
+function factorial(numb) {
+    return 0
+}
---
+++ jsProblems/recursion/fibonacciNumbers.js
@@ -0,0 +1,12 @@
+/**
+ * Return the n-th Fibonacci number
+ * For example: 
+ * fib(1) -> 1
+ * fib(3) -> 2
+ * fib(4) -> 3
+ * @param {*} n 
+ * @returns 
+ */
+function fib(n) {
+    return 0
+}
