9a995ffe0dd5c6d57e0f0257f79f7298cb96ef3b
guohao.yan <sccdmaomao@hotmail.com>
2022-11-02 18:22:20 -0400

    add capitalize problem

---
+++ jsProblems/string/capitalize.js
@@ -0,0 +1,11 @@
+/**
+ * Caplitalize every word's first chrarcter
+ * For example:
+ * capitalize("hello") -> "Hello"
+ * capitalize("hello world") -> "Hello World"
+ * @param {string} string string of any length
+ * @returns Capitalized words
+ */
+function capitalize(string) {
+    return ""
+}
