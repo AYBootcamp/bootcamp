0383785b324bba5ef5a7d3d14d9310813d3cecf3
guohao.yan <sccdmaomao@hotmail.com>
2023-01-14 17:58:08 -0500

    fix code factor

--- exercises/solutions/caffery/JS-Exercises-1.js
+++ exercises/solutions/caffery/JS-Exercises-1.js
@@ -16,7 +16,7 @@ let checkarray = [true, false, "oshdiuhr", 10, 15, 200, "si"]
 for (let i = 0; i < checkarray.length; i++) {
     if (typeof checkarray[i] === "string" && checkarray[i].length > 3) {
         console.log("true")
-    } else if (typeof checkarray[i] === "number" && checkarray[i] % 10 == 0) {
+    } else if (typeof checkarray[i] === "number" && checkarray[i] % 10 === 0) {
         console.log("true")
     } else if (typeof checkarray[i] === "boolean") {
         console.log("true")
