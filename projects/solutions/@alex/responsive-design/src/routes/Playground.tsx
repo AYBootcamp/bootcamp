82fec149df560b76b56887d04913e1fd9197a0ab
guohao.yan <sccdmaomao@hotmail.com>
2023-04-29 18:09:37 -0400

    add hide & show

--- projects/solutions/@alex/responsive-design/src/routes/Playground.tsx
+++ projects/solutions/@alex/responsive-design/src/routes/Playground.tsx
@@ -1,14 +1,66 @@
+import styled from '@emotion/styled'
+import { useState } from 'react'
+
 import Flexbox from './PlaygroundComponents/Flexbox'
 import Units from './PlaygroundComponents/Units'
 
+const CheckboxContainer = styled.div`
+    & > * {
+        margin: 0 5px;
+        text-transform: capitalize;
+    }
+`
+
+interface PlaygroundComponents {
+    flexBox: boolean
+    units: boolean
+}
+
 const Playground = () => {
+    const [shouldDisplayComponent, setShouldDisplayComponent] =
+        useState<PlaygroundComponents>({
+            flexBox: true,
+            units: true,
+        })
+
+    const componentCheckboxes: Array<keyof PlaygroundComponents> = [
+        'flexBox',
+        'units',
+    ]
     return (
         <div>
             <h1>Playground</h1>
-            <Flexbox />
-            <hr />
-            <Units />
-            <hr />
+            <CheckboxContainer>
+                <span>Show:</span>
+                {componentCheckboxes.map((component) => (
+                    <label key={component}>
+                        <input
+                            type="checkbox"
+                            checked={shouldDisplayComponent[component]}
+                            onChange={(e) =>
+                                setShouldDisplayComponent((state) => ({
+                                    ...state,
+                                    [component]: e.target.checked,
+                                }))
+                            }
+                        ></input>
+                        {component}
+                    </label>
+                ))}
+            </CheckboxContainer>
+
+            {shouldDisplayComponent.flexBox && (
+                <>
+                    <Flexbox />
+                    <hr />
+                </>
+            )}
+            {shouldDisplayComponent.units && (
+                <>
+                    <Units />
+                    <hr />
+                </>
+            )}
         </div>
     )
 }
